3c33ae2f4f817e194e726cb9dd3bf1f3b5a663a3
Diego Camino <diego.camino.r@gmail.com>
2020-05-29 23:21:34 -0500

    Refactoring

--- src/api.js
+++ src/api.js
@@ -1,40 +1,63 @@
 const url = "https://pokeapi.co/api/v2";
 
-function getPokemones() {
-  fetch(`${url}/pokemon?limit=151`) // &offset=20
-    .then((response) => response.json())
-    .then(function(allpokemon) {
-      // console.log(allpokemon);
-      let pokemons = [];
-      allpokemon.results.forEach(function(pokemon, index) {
-        pokemons.push({
-          id: index + 1,
-          name: pokemon.name,
-          url: pokemon.url,
-          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
-            1}.png`,
-        });
-      });
-      console.log(pokemons);
-      renderPokemons(pokemons);
-    });
+async function getPokemones() {
+  // fetch(`${url}/pokemon?limit=151`) // &offset=20
+  //   .then((response) => response.json())
+  //   .then(function(allpokemon) {
+  //     let pokemons = [];
+  //     allpokemon.results.forEach(function(pokemon, index) {
+  //       pokemons.push({
+  //         id: index + 1,
+  //         name: pokemon.name,
+  //         url: pokemon.url,
+  //         image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+  //           1}.png`,
+  //       });
+  //     });
+  //     renderPokemons(pokemons);
+  //   });
+
+  const res = await fetch(`${url}/pokemon?limit=20`);
+  const data = await res.json();
+  const pokemons = data.results.map((result, index) => ({
+    ...result,
+    id: index + 1,
+    image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index +
+      1}.png`,
+  }));
+  console.log("pokemon");
+  renderPokemons(pokemons);
+  // const pokemon = data.results
 }
 
 function renderPokemons(pokemons) {
   const pokedex = document.getElementById("container-pokedex");
-  const pokemonHTMLString = [];
-  pokemons.forEach(function(pokemon) {
-    pokemonHTMLString.push(
-      `
+  // const pokemonHTMLString = [];
+  // pokemons.forEach(function(pokemon) {
+  //   pokemonHTMLString.push(
+  //     `
+  //     <li class="card">
+  //       <img class="card-image" src="${pokemon.image}"/>
+  //       <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+  //     </li>
+  //     `
+  //   );
+  // });
+  // pokedex.innerHTML = pokemonHTMLString.join("");
+  const pokemonHTMLString = pokemons
+    .map(
+      (pokemon) =>
+        `
       <li class="card">
         <img class="card-image" src="${pokemon.image}"/>
         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
       </li>
       `
-    );
-  });
-  pokedex.innerHTML = pokemonHTMLString.join("");
+    )
+    .join("");
+  pokedex.innerHTML = pokemonHTMLString;
 }
+
 // function fetchPokemonData(pokemon) {
 //   let url = pokemon.url; // <--- this is saving the pokemon url to a variable to us in a fetch.(Ex: https://pokeapi.co/api/v2/pokemon/1/)
 //   fetch(url)
